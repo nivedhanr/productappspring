ecaf2d07980465c74f01fa3168a802c5b403754b
nivedhanr <nivedharenahan@gmail.com>
2022-12-06 21:40:57 +0530

    nav

--- src/app/addproduct/addproduct.component.ts
+++ src/app/addproduct/addproduct.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-addproduct',
@@ -14,9 +15,29 @@ export class AddproductComponent {
   price =""
   sellername =""
   distributor =""
+  constructor(private api:ApiService){}
   readValues=()=>
   {
     let data:any={"productcode":this. productcode,"productname":this.productname,"mandate":this.mandate,"expdate":this.expdate,"brand":this.brand,"price":this.price,"sellername":this.sellername,"distributor":this.distributor}
     console.log(data)
+    this.api.addproduct(data).subscribe(
+      (response:any)=>{
+        console.log(response)
+        if (response.status == "success") {
+          alert("Course added successfully")
+          this.productcode=""
+          this.productname=""
+          this.mandate=""
+          this.expdate=""
+          this.brand=""
+          this.price=""
+          this.sellername=""
+          this.distributor
+         
+        }else{
+          alert("Something went wrong")
+        }
+        }
+    )
 }
 }
